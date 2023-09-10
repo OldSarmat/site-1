8bf4253746d1a2ae012d6b801b61d408cfa332ba
OldSarmat <sarm.dn@yandex.ru>
2023-09-10 20:19:36 +0300

    fix-bag

--- js/main.js
+++ js/main.js
@@ -2,13 +2,16 @@ $(document).ready(function () {
 	// Плавная прокрутка меню
 	$('nav a[href^="#"]').click(function () {
 		let target = $(this).attr('href');
-		$('html, body').animate({
-			scrollTop: $(target).offset().top
-		}, 1500);
+		$('html, body').animate(
+			{
+				scrollTop: $(target).offset().top,
+			},
+			1500
+		);
 		$('nav a[href^="#"]').parent().removeClass('active');
 		$(this).parent().addClass('active');
-		$('.menu').toggle(500);
 		$('.menu__burger').toggleClass('close');
+		// $('.menu').toggle(500);
 		return false;
 	});
 	// Мобильное меню
